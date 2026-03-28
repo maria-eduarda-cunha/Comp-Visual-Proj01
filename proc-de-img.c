9cf7a2fa5eb3d446026783fa717f92815a13cc7a
Arthur Furukawa <arthurrfurukawa@hotmail.com>
2026-03-28 00:16:47 -0300

    Parte 1 e 2 finalizadas
    Adição da parte 2 do projeto, conversão para escala de cinza.

--- proc-de-img.c
+++ proc-de-img.c
@@ -15,10 +15,9 @@ if (!SDL_Init(SDL_INIT_VIDEO)){
     return 1;
 }
 
-
 const char* caminho_imagem = argv[1];
 
-// Carrega a imagem em PNG, JPG, BMP asas a
+// Carrega a imagem em PNG, JPG, BMP
 SDL_Surface* surface_original = IMG_Load(caminho_imagem);
 
 // Caso o arquivo não seja suportado, faz o tratamento sem 
@@ -41,4 +40,46 @@ if(!imagem){
     SDL_Quit();
     return 1;
 }
+
+// Analise da imagem (ver se é colorida)
+bool colorida = false;
+Uint32* pixels = (Uint32*)imagem->pixels;
+int total_pixels = imagem-> w * imagem->h;
+
+
+const SDL_PixelFormatDetails* detalhes = SDL_GetPixelFormatDetails(imagem->format);
+
+// Se RGB igual, então imagem colorida
+for(int i = 0; i < total_pixels; ++i){
+    Uint8 r, g, b, a;
+    SDL_GetRGBA(pixels[i], detalhes, NULL, &r, &g, &b, &a);
+
+    if(r != g || r != b || g != b) {
+        colorida = true;
+        break;
+        }
+    }
+
+    // Conversão para escala de cinza
+    if(colorida){
+        printf("Imagem Colorida. Fazendo conversão para escala de cinza!\n");
+
+        for(int i = 0; i < total_pixels; ++i){
+            Uint8 r, g, b, a;
+            SDL_GetRGBA(pixels[i], detalhes, NULL, &r, &g, &b, &a);
+
+            // Formula para conversão
+            Uint8 y = (Uint8)((0.2125 * r) + (0.7154 * g) + (0.0721 * b));
+
+            pixels[i] = SDL_MapRGBA(detalhes, NULL, y, y, y, a);
+        }
+        printf("Conversão Finalizada\n");
+    } else {
+        printf("Imagem convertida para escala de cinza!\n");
+    }
+
+SDL_DestroySurface(imagem);
+SDL_Quit();
+
+return 0;
 }
